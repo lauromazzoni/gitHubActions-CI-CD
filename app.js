80a5361ff80adda7d0f2c5d0c118b27ed3ec3964
lauromazzoni <lauromazzoni@gmail.com>
2024-06-20 13:49:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Primeiro commit <br/> Feature XPTO <br/> Feature 1234')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
