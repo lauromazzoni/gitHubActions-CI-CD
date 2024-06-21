6c3e9d3afe2ae8d0659d0973d5c2ba5be1476e21
lauromazzoni <lauromazzoni@gmail.com>
2024-06-21 10:35:17 -0300

    teste output

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Primeiro commit <br/> Feature XPTO <br/> Feature 1234 <br/> Funfou de novo!!!')
+  res.send('Primeiro commit <br/> Feature XPTO <br/> Feature 1234 <br/> Funfou de novo!!!<br/><br/><br/>Mais uma vez!!!')
 })
 
 app.listen(port, () => {
